572d3e98d06dbfbbe8330e27057570dac36ec57a
Quy Phan Quang <quypq.dev@gmail.com>
2024-12-22 13:20:41 +0700

    feat: add web socket connection

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import express from 'express';
+import { Server } from 'socket.io';
+import http from 'http';
+import cors from 'cors';
 
 const app = express();
-const port = 4000;
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000', // Your frontend origin
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const port = 4000;
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
